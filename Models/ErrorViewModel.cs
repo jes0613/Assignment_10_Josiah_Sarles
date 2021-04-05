1fb0eec2715ae46553037b68d8a5fb250197ce36
jes0613 <46938121+jes0613@users.noreply.github.com>
2021-04-05 15:32:47 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment_10_Josiah_Sarles.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
